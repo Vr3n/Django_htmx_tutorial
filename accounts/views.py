fee597a88983046019beca9fe2d2a2d77f0a7a20
vr3n <vickyspatel@gmail.com>
2021-08-23 15:35:13 +0530

    created user login form

--- accounts/views.py
+++ accounts/views.py
@@ -1,37 +1,42 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 # Create your views here.
+
 
 def login_view(request):
     if request.user.is_authenticated:
         return redirect("/")
-    
 
+    form = AuthenticationForm(request)
     if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
+        form = AuthenticationForm(request, data=request.POST)
 
-        user = authenticate(request, username=username, password=password)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
 
-        if user is None:
-            context = {"error": "Invalid username or password"}
-            return render(request, "accounts/login.html", context)
+        # If the url consists of 'next' param.
+        # redirect to the next="url".
 
-        login(request, user)
         if request.GET.get('next'):
             next_url = request.GET.get('next')
             return redirect(next_url)
-        
+
+        # redirect to home page.
         return redirect('/')
+
+    # Return for get request.
     return render(request, "accounts/login.html", {})
+
 
 def logout_view(request):
     if request.method == "POST":
         logout(request)
         return redirect('/accounts/login/')
     return render(request, "accounts/logout.html", {})
+
 
 def register_view(request):
     if request.user.is_authenticated:
@@ -41,5 +46,5 @@ def register_view(request):
     if form.is_valid():
         user_obj = form.save()
         return redirect('/accounts/login/')
-    context = { "form": form }
+    context = {"form": form}
     return render(request, "accounts/register.html", context)
